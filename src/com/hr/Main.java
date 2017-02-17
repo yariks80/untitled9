7e1a0a4e2f851644f13c925e2cf87247f0610115
Iaroslav Sviatkin <yariks@i.ua>
2017-02-17 11:37:33 +0200

    Initial commit

---
+++ src/com/hr/Main.java
@@ -0,0 +1,14 @@
+package com.hr;
+
+/**
+ * Created by Administrator on 2/17/2017.
+ */
+public class Main {
+    public static void main(String[] args) {
+        for (int j = 0; j < 20; j++) {
+            if (j % 2 == 0)
+                System.out.println("j = " + j);
+
+        }
+    }
+}
