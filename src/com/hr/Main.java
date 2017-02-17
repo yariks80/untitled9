afe1b289ab47cc6aa112f3fa7a39dd829b3a4080
Iaroslav Sviatkin <yariks@i.ua>
2017-02-17 11:48:08 +0200

    add string System

--- src/com/hr/Main.java
+++ src/com/hr/Main.java
@@ -8,6 +8,7 @@ public class Main {
         for (int j = 0; j < 20; j++) {
             if (j % 2 == 0)
                 System.out.println("j = " + j);
+                System.out.println();
 
         }
     }
